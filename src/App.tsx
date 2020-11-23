4e4038586093f209380a6427576038f034adbd9a
alexis-savoie <alexis.savoie.555@gmail.com>
2020-11-23 16:30:01 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  testAxios()
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,11 +20,25 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (TEST)
         </a>
       </header>
     </div>
   );
+
+  
 }
+
+
+function testAxios() {
+  axios.get(`http://localhost:8020/test`)
+      .then(res => {
+          console.log("Ceci est un test !")
+          console.log(res.data)
+      })
+  }
+
+
+
 
 export default App;
